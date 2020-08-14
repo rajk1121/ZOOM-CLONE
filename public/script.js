b7b42ef26b339c4c6a1d5ecfe367e8220aef44f1
Rajk11 <rajat.mishra1121@gmail.com>
2020-08-14 20:13:40 +0530

    initial

---
+++ public/script.js
@@ -0,0 +1,51 @@
+console.log("geheehhe")
+const myVideo = document.createElement("video")
+myVideo.muted = true
+const roomId = "<%= roomId %>"
+let myVideoStream;
+let myId
+const socket = io()
+var peer = new Peer(undefined, {
+    path: '/peer',
+    host: '/',
+    port: 3000
+});
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then((stream) => {
+    myVideoStream = stream
+    addVideoStream(myVideo, stream)
+
+    peer.on('call', (call) => {
+        call.answer(stream); // Answer the call with an A/V stream.
+        // console.log(otherId)
+        const video = document.createElement("video")
+        call.on('stream', function (remoteStream) {
+            // Show stream in some video/canvas element.
+            addVideoStream(video, remoteStream)
+        });
+    })
+
+    socket.on("user-connected", (userId) => {
+        var call = peer.call(userId, stream);
+        const video = document.createElement("video")
+        call.on('stream', function (remoteStream) {
+            addVideoStream(video, remoteStream)
+        });
+        console.log("user joined", userId)
+    })
+})
+peer.on('open', id => {
+    console.log(id)
+    myId = id
+    socket.emit('joined-room', roomId, id)
+})
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+        // console.log("vdee")
+    })
+    document.querySelector("#video-grid").append(video)
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server);
+const { v4: uuidv4 } = require("uuid");
+const { ExpressPeerServer } = require('peer')
+const peerServer = ExpressPeerServer(server,
+    { debug: true }
+)
+app.use(express.static('public'))
+app.use('/peer', peerServer)
+io.on('connection', socket => {
+    socket.on('joined-room', (roomId, id) => {
+        console.log("cefe")
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit("user-connected", id)
+    })
+})
+app.set("view engine", "ejs")
+app.get('/', (req, res) => {
+    const uuid = uuidv4();
+    res.redirect(`/${uuid}`)
+})
+app.get("/:room", (req, res) => {
+    res.render("room", { roomId: req.params.room })
+})
+server.listen(3000, () => {
+    console.log("Server started at 3000")
+});
